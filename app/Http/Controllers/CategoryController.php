944a3d41c47b44f83abf233d90e00d2489304c5a
rortega8906 <raortega8906@gmail.com>
2025-07-08 01:40:42 +0200

    feat: finish crud category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,117 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
+
 class CategoryController extends Controller
 {
 
-    
-    
+    public function index()
+    {
+        $categories = Category::all();
+
+        if($categories->isEmpty()) {
+            return response()->json([
+
+                'status' => 200,
+                'message' => 'No se ha registrado ninguna Categoría aún.',
+
+            ], 200);
+        }
+        
+        return response()->json([
+
+            'status' => 200,
+            'message' => 'Categorías encontradas.',
+            'data' => $categories
+
+        ], 200);
+    }
+
+    public function store(StoreCategoryRequest $request)
+    {
+        $validated = $request->validated();
+
+        $category = Category::create($validated);
+        return response()->json([
+
+            'status' => 201,
+            'message' => 'Categoría creada correctamente',
+            'data' => $category
+
+        ], 201);
+    }
+
+    public function show($id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json([
+
+                'status' => 404,
+                'message' => 'Categoría no encontrada.'
+
+            ], 404);
+        }
+
+        return response()->json([
+
+            'status' => 200,
+            'message' => 'Categoría encontrada.',
+            'data' => $category
+
+        ], 200);
+    }
+
+    public function update($id, UpdateCategoryRequest $request)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json([
+
+                'status' => 404,
+                'message' => 'Categoría no encontrada.'
+
+            ], 404);
+        }
+
+        $validated = $request->validated();
+
+        $category->update($validated);
+        return response()->json([
+
+            'status' => 200,
+            'message' => 'Categoría actualizada correctamente',
+            'data' => $category
+
+        ], 200);
+    }
+
+    public function destroy($id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json([
+
+                'status' => 404,
+                'message' => 'Categoría no encontrada.'
+
+            ], 404);
+        }
+
+        $category->delete();
+        return response()->json([
+
+            'status' => 200,
+            'message' => 'Categoría eliminada correctamente.',
+            'data' => $category
+
+        ], 200);
+    }
+
 }
--- app/Http/Requests/UpdateCategoryRequest.php
+++ app/Http/Requests/UpdateCategoryRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateCategoryRequest extends FormRequest
@@ -11,7 +13,7 @@ class UpdateCategoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -19,10 +21,28 @@ class UpdateCategoryRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
+
     public function rules(): array
     {
         return [
-            //
+            'name' => 'string|unique:categories'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => 422,
+            'message' => 'Error en la validación de datos.',
+            'errors' => $validator->errors()
+        ], 422));
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.string' => 'El nombre de la categoría debe existir y/o ser una cadena de texto.',
+            'name.unique' => 'El nombre de la categoría ya existe.'
         ];
     }
 }
