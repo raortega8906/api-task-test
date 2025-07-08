63b57d61d06c962050dcc87e07a93cd776aa8d73
rortega8906 <raortega8906@gmail.com>
2025-07-09 00:24:53 +0200

    feat: update requests

--- app/Http/Requests/StoreCategoryRequest.php
+++ app/Http/Requests/StoreCategoryRequest.php
@@ -28,6 +28,13 @@ class StoreCategoryRequest extends FormRequest
         ];
     }
 
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     * @throws \Illuminate\Http\Exceptions\HttpResponseException
+     */
+    
     public function failedValidation(Validator $validator)
     {
         throw new HttpResponseException(response()->json([
