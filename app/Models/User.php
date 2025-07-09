d0d22be908dc0e8f1652c6cabdbf2e18929c157b
rortega8906 <raortega8906@gmail.com>
2025-07-09 23:34:38 +0200

    feat: add jwt to project

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,26 @@ class User extends Authenticatable
         ];
     }
 
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
     public function categories()
     {
         return $this->hasMany(Category::class);
